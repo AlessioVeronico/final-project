a9343f6a95cfa530e10f7af943fb7aeb17838784
Alessio Veronico <averonico.dev@gmail.com>
2021-10-06 16:00:25 +0200

    imported imgs in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import react from '../../images/react.png'
+import wp from '../../images/wp.png'
 
 export default class Home extends React.Component {
   render() {
     return(
-      <div className="alert alert-primary" role="alert">
-        This is the header
+      <div className='text-center h-25'>
+        <img className="mh-25 mw-25" src={ react } alt="react" />
+        <img className="mh-25 mw-25" src={ wp } alt="react" />
       </div>
     )
   }

4f9f70f381e632f20f522188176880a8c0ce53d5
Alessio Veronico <averonico.dev@gmail.com>
2021-10-06 16:00:33 +0200

    updated footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default class Home extends React.Component {
   render() {
     return(
-      <div className="alert alert-primary" role="alert">
-        This is the footer
+      <div className="bg-dark text-white text-center p-2 m-2" >
+        <p>Final Project Epicode School </p> 
+        <p>Property of Alessio Veronico </p> 
+        <p>averonico.dev@gmail.com </p> 
       </div>
     )
   }

61182ce6eea8d5ed6766c9405c1c71b8fcbd18c4
Alessio Veronico <averonico.dev@gmail.com>
2021-10-06 21:21:25 +0200

    modified model for Post, added slug and img

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,15 +1,14 @@
 export class Post {
-  constructor(id, title, content, imgPath, link) {
+  constructor(id, title, content, excerpt, slug, img) {
     this.content = content;
+    this.excerpt = excerpt;
     this.id = id;
-    // this.imgPath = imgPath;
-    // this.link = link;
+    this.img = img
+    this.slug = slug;
     this.title = title;
   }
 }
   
 export function getPost(post) {
-  return new Post(post.id, post.title.rendered, post.content.rendered, 
-    //post.imgPath, post.link
-    );
+  return new Post( post.id, post.title.rendered, post.content.rendered, post.excerpt.rendered, post.slug, post.better_featured_image.media_details.sizes.thumbnail.source_url );
 }

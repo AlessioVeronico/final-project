8a2c200615ad6ef6c032d5666f732e20dc8cb937
Alessio Veronico <averonico.dev@gmail.com>
2021-10-06 12:29:49 +0200

    temp. solution to print "about us" page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { getPage } from "../../models/Page";
 
 export default class About extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+			pages: []
+		};
   }
 
+  componentDidMount() {
+    fetch('http://laragon.test/bedrock/web/wp-json/wp/v2/pages/').then(
+      res => res.json()
+    ).then(
+      pages => this.setState({
+        pages: pages.filter( page => page.slug == 'about-us').map( page => getPage(page) )
+      })
+    );
+    }
+
   render() {
+    const page = this.state.pages.map( page => page.content
+      );
     return(
-      <div className="alert alert-warning" role="alert">
-        {/* { this.props.page.content } */}
-      </div>
+      <div dangerouslySetInnerHTML={{__html: page}} />
+       
     )
   }
 }
